16ff13abc6db2fa6d02e6d3f33a3e9c9ee24b54a
AshedFox <a.mahn@mail.ru>
2026-03-27 15:34:32 +0300

    feat(shared): add common and auth-related shared zod schemas

---
+++ packages/shared/src/auth/schemas.ts
@@ -0,0 +1,39 @@
+import z from 'zod';
+import { errorSchema } from '../common';
+import { ACCESS_TOKEN_ERRORS } from './constants';
+
+const authResponseSchema = z.object({
+  accessToken: z.string(),
+  expiresAt: z.iso.datetime(),
+});
+
+export const loginBodySchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(8)
+    .regex(/[a-zA-Z]/, {
+      error: 'Password must contain at least one letter.',
+    })
+    .regex(/[0-9]/, { error: 'Password must contain at least one number.' }),
+});
+
+export const loginResponseSchema = authResponseSchema;
+
+export const registerBodySchema = loginBodySchema
+  .extend({
+    passwordComparison: z.string(),
+    name: z.string().min(2).max(127),
+  })
+  .refine((data) => data.password === data.passwordComparison, {
+    message: 'Passwords do not match',
+    path: ['passwordComparison'],
+  });
+
+export const registerResponseSchema = authResponseSchema;
+
+export const refreshResponseSchema = authResponseSchema;
+
+export const accessTokenErrorSchema = errorSchema.extend({
+  code: z.enum(ACCESS_TOKEN_ERRORS),
+});
