bc3043fe97b2caa65eb156b83c0a7e005b85b999
AshedFox <a.mahn@mail.ru>
2026-04-03 19:38:23 +0300

    feat(api): add status to users table

--- apps/api/src/db/schema/users.ts
+++ apps/api/src/db/schema/users.ts
@@ -1,4 +1,12 @@
+import { pgEnum } from 'drizzle-orm/pg-core';
 import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const userStatusEnum = pgEnum('user_status', [
+  'UNCONFIRMED',
+  'CONFIRMED',
+]);
+
+export type UserStatus = (typeof userStatusEnum.enumValues)[number];
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
@@ -7,6 +15,7 @@ export const users = pgTable('users', {
   name: varchar('name', { length: 127 }).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  status: userStatusEnum('status').notNull().default('UNCONFIRMED'),
 });
 
 export type User = typeof users.$inferSelect;
