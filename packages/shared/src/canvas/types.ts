a6d2a3a978886b31780e628a705ba040d3f5ea46
AshedFox <a.mahn@mail.ru>
2026-04-04 22:18:56 +0300

    chore(shared): add canvas snapshot params type and schema

--- packages/shared/src/canvas/types.ts
+++ packages/shared/src/canvas/types.ts
@@ -1,5 +1,6 @@
 import z from 'zod';
 import {
+  canvasSnapshotParamsSchema,
   cooldownResponseSchema,
   pixelInfoParamsSchema,
   pixelInfoResponseSchema,
@@ -12,3 +13,4 @@ export type PixelsUpdateData = z.infer<typeof pixelsUpdateSchema>;
 export type CooldownResponse = z.infer<typeof cooldownResponseSchema>;
 export type PixelInfoParams = z.infer<typeof pixelInfoParamsSchema>;
 export type PixelInfoResponse = z.infer<typeof pixelInfoResponseSchema>;
+export type CanvasSnapshotParams = z.infer<typeof canvasSnapshotParamsSchema>;
