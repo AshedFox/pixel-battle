4ca2d77179e68c026c28632d10b7dac469e5ec7f
AshedFox <a.mahn@mail.ru>
2026-04-06 20:52:23 +0300

    feat(web): add tabbed board views for admin on home page

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,26 +1,84 @@
 import { useAuth } from '@/components/AuthProvider';
+import { HistoryBoard } from '@/components/HistoryBoard';
 import { PixelBoard } from '@/components/PixelBoard';
 import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { HistoryIcon, SwordIcon } from 'lucide-react';
+import { useCallback, useMemo } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+type Mode = 'history' | 'board';
 
 export const Home = () => {
-  const { logout } = useAuth();
+  const { logout, role } = useAuth();
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const mode = useMemo<Mode>(() => {
+    const mode = searchParams.get('mode');
+
+    if (mode === 'history') {
+      return 'history';
+    }
+
+    return 'board';
+  }, [searchParams]);
 
   const handleLogout = async () => {
     await logout();
     navigate('/login', { replace: true });
   };
 
+  const handleModeChange = useCallback(
+    (mode: string) => {
+      setSearchParams((prev) => {
+        if (mode === 'history') {
+          prev.set('mode', mode);
+        } else {
+          prev.delete('mode');
+        }
+        return prev;
+      });
+    },
+    [setSearchParams],
+  );
+
   return (
-    <div className="flex flex-col w-screen h-screen">
+    <div className="flex flex-col w-dvw h-dvh">
       <header className="p-4 flex justify-between border-b">
         <h1 className="text-3xl font-bold">Pixel Battle</h1>
-        <Button onClick={handleLogout} variant="destructive">
-          Logout
-        </Button>
+        <div className="flex items-center gap-2">
+          <Button onClick={handleLogout} variant="destructive">
+            Logout
+          </Button>
+        </div>
       </header>
-      <PixelBoard />
+      <main className="flex flex-col flex-1">
+        {role === 'ADMIN' ? (
+          <Tabs
+            value={mode}
+            onValueChange={handleModeChange}
+            className="flex-1 flex flex-col p-2"
+          >
+            <TabsList className="self-center max-w-lg w-full">
+              <TabsTrigger value="board">
+                <SwordIcon /> Board
+              </TabsTrigger>
+              <TabsTrigger value="history">
+                <HistoryIcon /> History
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="board" className="flex flex-col">
+              <PixelBoard />
+            </TabsContent>
+            <TabsContent value="history" className="flex flex-col">
+              <HistoryBoard />
+            </TabsContent>
+          </Tabs>
+        ) : (
+          <PixelBoard />
+        )}
+      </main>
     </div>
   );
 };
