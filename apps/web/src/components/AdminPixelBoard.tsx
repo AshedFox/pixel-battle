bdd6dc280daf63fa66b00ab99bed1aaf26f47bfc
AshedFox <a.mahn@mail.ru>
2026-04-10 20:53:58 +0300

    feat(web): add UI and logic for rect drawing to AdminPixelBoard

--- apps/web/src/components/AdminPixelBoard.tsx
+++ apps/web/src/components/AdminPixelBoard.tsx
@@ -1,9 +1,12 @@
-import { use, useMemo, useRef, useState } from 'react';
+import { use, useCallback, useMemo, useRef, useState } from 'react';
 import { AdminHeatmap, AdminHeatmapHandle } from './AdminHeatmap';
 import { Button } from './ui/button';
 import { PixelStream } from '@/lib/pixel-stream';
 import { BasePixelBoard } from './BasePixelBoard';
 import { usePixelBoard } from '@/hooks/usePixelBoard';
+import { Selection, useDrawRect } from '@/hooks/useDrawRect';
+import { Field } from './ui/field';
+import { MAX_FILL_AREA } from '@repo/shared';
 
 export const AdminPixelBoard = ({
   canvasPromise,
@@ -14,16 +17,60 @@ export const AdminPixelBoard = ({
   const adminHeatmapRef = useRef<AdminHeatmapHandle>(null);
   const pixelStream = useMemo(() => new PixelStream(), []);
   const [heatmapEnabled, setHeatmapEnabled] = useState(false);
+  const selectionRef = useRef<Selection>(null);
   const base = usePixelBoard({
     initialData,
     onViewportChange: () => adminHeatmapRef.current?.redrawHeatmap(),
     onPixelUpdate: (x, y) => pixelStream.emit(x, y),
+    selectionRef,
   });
   const { viewportRef, containerRef } = base.refs;
+  const { scheduleRedraw } = base;
+
+  const drawRect = useDrawRect({
+    viewportRef,
+    scheduleRedraw,
+    selectionRef,
+    onFill: base.drawRect,
+  });
+
+  const handleMouseUp = useCallback(() => {
+    drawRect.handlers.onMouseUp();
+
+    base.canvasHandlers.onMouseUp();
+  }, [base.canvasHandlers, drawRect]);
+
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement>) => {
+      if (drawRect.active) {
+        drawRect.handlers.onMouseMove(e);
+      }
+
+      base.canvasHandlers.onMouseMove(e);
+    },
+    [base.canvasHandlers, drawRect],
+  );
+
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement>) => {
+      if (drawRect.active && e.button === 0) {
+        drawRect.handlers.onMouseDown(e);
+        return;
+      }
+
+      base.canvasHandlers.onMouseDown(e);
+    },
+    [base.canvasHandlers, drawRect],
+  );
 
   return (
     <BasePixelBoard
       base={base}
+      canvasHandlers={{
+        onMouseUp: handleMouseUp,
+        onMouseMove: handleMouseMove,
+        onMouseDown: handleMouseDown,
+      }}
       overlaysSlot={
         <AdminHeatmap
           ref={adminHeatmapRef}
@@ -34,12 +81,52 @@ export const AdminPixelBoard = ({
         />
       }
       controlsSlot={
-        <Button
-          variant="outline"
-          onClick={() => adminHeatmapRef.current?.toggleHeatmap()}
-        >
-          {heatmapEnabled ? 'Hide Heatmap' : 'Show Heatmap'}
-        </Button>
+        <Field>
+          <Button
+            variant="outline"
+            onClick={() => adminHeatmapRef.current?.toggleHeatmap()}
+          >
+            {heatmapEnabled ? 'Hide Heatmap' : 'Show Heatmap'}
+          </Button>
+          {drawRect.active ? (
+            <Field className="flex-1" orientation="horizontal">
+              <Button
+                className="grow"
+                onClick={drawRect.confirm}
+                disabled={
+                  !drawRect.selectionSize ||
+                  drawRect.selectionSize.w * drawRect.selectionSize.h >
+                    MAX_FILL_AREA
+                }
+              >
+                {!drawRect.selectionSize
+                  ? 'Select area...'
+                  : drawRect.selectionSize.w * drawRect.selectionSize.h >
+                      MAX_FILL_AREA
+                    ? 'Area too big'
+                    : `Fill ${drawRect.selectionSize.w}x${drawRect.selectionSize.h}`}
+              </Button>
+              <Button
+                className="grow"
+                variant="outline"
+                onClick={drawRect.deactivate}
+              >
+                Cancel
+              </Button>
+            </Field>
+          ) : (
+            <Button
+              variant="outline"
+              onClick={() => {
+                base.handlePixelPopoverClose();
+                base.setPendingPixel(null);
+                drawRect.activate();
+              }}
+            >
+              Fill Area
+            </Button>
+          )}
+        </Field>
       }
     />
   );
